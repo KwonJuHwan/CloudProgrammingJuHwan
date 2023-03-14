52a3b0291b4b380513909dee9776756e0450cbaa
201810900KwonJuHwan <kjh307ok@naver.com>
2023-03-14 17:48:25 +0900

    blog/Post 모델생성

---
+++ django_project/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Post
+
+# Register your models here.
+
+admin.site.register(Post) #포스트 추가
+
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'blog',
+    'single_pages',
     'django.contrib.staticfiles',
 ]
 
